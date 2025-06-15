4c993afe745b72ff62a579f53b89f30c48a83024
William Beaudin <williambeaudun@gmail.com>
2025-06-15 16:11:50 -0400

    Refactor main.dart: update imports and enhance App class structure; integrate AppContext for improved connection handling.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,32 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'src/app.dart';
+import 'package:gitnar/src/context/app_context.dart';
+import 'package:gitnar/src/routes.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load();
+  await AppContext.instance.load();
+
   runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final bool connected = AppContext.instance.isFullyConnected;
+
+    return MaterialApp(
+      title: "GitNar",
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: connected ? Routes.home : Routes.auth,
+      routes: appRoutes,
+      debugShowCheckedModeBanner: false,
+    );
+  }
 }
