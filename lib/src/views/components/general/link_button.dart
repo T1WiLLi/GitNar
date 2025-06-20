9a637fc199fecc05b73a537b672e049d24b0d4d7
Sam <samuelcourchesne2004@gmail.com>
2025-06-19 22:36:13 -0400

    feat: add step and link to the git repo

--- lib/src/views/components/general/link_button.dart
+++ lib/src/views/components/general/link_button.dart
@@ -2,12 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class LinkButton extends StatelessWidget {
-  final Uri _url = Uri.parse('https://flutter.dev');
+  final Uri _url;
+  final String _buttonText;
 
-
+  const LinkButton({super.key, required String buttonText, required Uri url})
+    : _buttonText = buttonText,
+      _url = url;
 
   Future<void> _launchUrl() async {
-    if (!await launchUrl(_url)) {
+    if (!await launchUrl(_url, mode: LaunchMode.externalApplication)) {
       throw 'Could not launch $_url';
     }
   }
@@ -16,7 +19,16 @@ class LinkButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return TextButton(
       onPressed: _launchUrl,
-      child: const Text('Aller sur Flutter.dev'),
+      child: Text(
+        _buttonText,
+        style: const TextStyle(
+          color: Colors.blue,
+          fontSize: 14,
+          decoration: TextDecoration.underline,
+          fontWeight: FontWeight.normal,
+          ),
+      ),
+      
     );
   }
 }
--- lib/src/views/components/home_view/information_icon_modal.dart
+++ lib/src/views/components/home_view/information_icon_modal.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gitnar/src/views/components/general/link_button.dart';
 
 class InformationIconModal extends StatelessWidget {
   const InformationIconModal({super.key});
@@ -21,11 +22,21 @@ class InformationIconModal extends StatelessWidget {
           ],
         ),
         child: Column(
-          mainAxisSize: MainAxisSize.min,
           children: [
             _buildHeader(),
             const SizedBox(height: 16),
-            _buildContent(),
+
+            // <<< make this scrollable >>>
+            Expanded(
+              child: SingleChildScrollView(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [_buildContent()],
+                ),
+              ),
+            ),
+
             const SizedBox(height: 16),
             _buildFooter(context),
           ],
@@ -35,13 +46,57 @@ class InformationIconModal extends StatelessWidget {
   }
 
   Widget _buildContent() {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Text(
-        'GitNar is a tool designed to help you manage your Git and Sonar repositories more effectively. '
-        'It provides a user-friendly interface to view and manage your repositories, track changes, and collaborate with your team.',
-        style: const TextStyle(color: Colors.white, fontSize: 16),
-        textWidthBasis: TextWidthBasis.longestLine,
+    return Container(
+      padding: const EdgeInsets.all(24),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'Gitnar is a powerful tool that helps you manage your Git and Sonar repositories with ease. To get started, follow these simple steps:',
+            style: TextStyle(fontSize: 16, color: Colors.white),
+          ),
+          const SizedBox(height: 40),
+          buildStepItem(
+            step: 1,
+            title: 'Select a Repository',
+            description:
+                'Go in the "Repositories" tab and select a repository to link.',
+          ),
+          const SizedBox(height: 32),
+          buildStepItem(
+            step: 2,
+            title: 'Authorize Access',
+            description:
+                'Click “Authorize” on GitNar to grant access to your selected repository.',
+          ),
+          const SizedBox(height: 32),
+          buildStepItem(
+            step: 3,
+            title: 'Complete Authorization',
+            description:
+                'Follow the prompts to complete the authorization process. This may involve logging into your Git account and confirming permissions.',
+          ),
+          const SizedBox(height: 32),
+          buildStepItem(
+            step: 4,
+            title: 'Follow the Prompts',
+            description:
+                'Once authorized, follow any additional prompts to finalize the setup. This may include selecting specific branches or configuring settings.',
+          ),
+          const SizedBox(height: 32),
+          Row(
+            children: [
+              Text(
+                'For more information about GitNar, visit our repository:',
+                style: const TextStyle(fontSize: 16, color: Colors.white),
+              ),
+              LinkButton(
+                buttonText: 'Click here',
+                url: Uri.parse('https://github.com/T1WiLLi/GitNar.git'),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
@@ -57,7 +112,7 @@ class InformationIconModal extends StatelessWidget {
           TextButton(
             onPressed: () => Navigator.of(context).pop(),
             style: TextButton.styleFrom(foregroundColor: Colors.white70),
-            child: const Text('Cancel'),
+            child: const Text('Close'),
           ),
         ],
       ),
@@ -95,6 +150,55 @@ class InformationIconModal extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+
+  Widget buildStepItem({
+    required int step,
+    required String title,
+    required String description,
+  }) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          width: 32,
+          height: 32,
+          decoration: BoxDecoration(
+            color: const Color(0xFF10B981),
+            shape: BoxShape.circle,
+          ),
+          alignment: Alignment.center,
+          child: Text(
+            '$step',
+            style: const TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        const SizedBox(width: 16),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(height: 4),
+              Text(
+                description,
+                style: const TextStyle(fontSize: 14, color: Colors.white70),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
