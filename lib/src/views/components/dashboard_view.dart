6377b3e11aa8e34a5591b82e5ae773c2486b6cf3
Sam <samuelcourchesne2004@gmail.com>
2025-06-21 11:48:02 -0400

    feat dashboard_view: add cards layout and initial design

--- lib/src/views/components/dashboard_view.dart
+++ lib/src/views/components/dashboard_view.dart
@@ -1,3 +1,5 @@
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
 
 class DashboardView extends StatelessWidget {
@@ -6,11 +8,76 @@ class DashboardView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.all(16),
       color: Colors.transparent,
-      child: const Center(
-        child: Text(
-          'Dashboard Placeholder',
-          style: TextStyle(color: Colors.white, fontSize: 20),
+      child: Column(
+        children: [
+          Center(child: _buildInformationCard()),
+          SizedBox(height: 16),
+          Center(child: _buildRecentActivityCard()),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildInformationCard() {
+    return Row(
+      children: [
+        Expanded(
+          flex: 1,
+          child: Container(
+            height: 150,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Color(0xFF3A4052), Color(0xFF2D3142)],
+                transform: GradientRotation(145 * math.pi / 180),
+              ),
+              borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+        ),
+        SizedBox(width: 16),
+        Expanded(
+          flex: 1,
+          child: Container(
+            height: 150,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Color(0xFF3A4052), Color(0xFF2D3142)],
+                transform: GradientRotation(145 * math.pi / 180),
+              ),
+              borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+        ),
+        SizedBox(width: 16),
+        Expanded(
+          flex: 1,
+          child: Container(
+            height: 150,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Color(0xFF3A4052), Color(0xFF2D3142)],
+                transform: GradientRotation(145 * math.pi / 180),
+              ),
+              borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildRecentActivityCard() {
+    return Expanded(
+      child: Container(
+        height: 500,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFF3A4052), Color(0xFF2D3142)],
+            transform: GradientRotation(145 * math.pi / 180),
+          ),
+          borderRadius: BorderRadius.circular(12),
         ),
       ),
     );
